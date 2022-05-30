d5c7351b624a806d2bcbfa957d9f68b7c9012c69
ppopdesk <97980419+ppopdesk@users.noreply.github.com>
2022-05-30 21:58:37 +0530

    fin

--- untitled folder/Assignment1.py
+++ untitled folder/Assignment1.py
@@ -18,8 +18,3 @@ end_time = datetime.now()
 print(f"Number to be appended to input string : {i}")
 print(f"Hash of resulting string : {hashlib.sha256(copy_of_input_string.encode('utf-8')).hexdigest()}")
 print(f"Time taken for program to run : {end_time-start_time}")
-
-"""After importing the required libraries, we ask for string input from user.
-We then initialise the target variable which is a integer (converted from hexadecimal string to base 10 integer)
-main_hexa variable is the sha256 output of the input string and copy variable is the copy of the input string. 
-We append numbers starting from 0 to the copy string and convert it to hex and check whether it is less than target variable. If yes, that is the number we are looking for."""
