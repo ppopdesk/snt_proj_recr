26282ca2f48968a3417a0d1ef4fadbc9a4d107a1
ppopdesk <97980419+ppopdesk@users.noreply.github.com>
2022-05-30 21:37:27 +0530

    Update Assignment1.py

--- Assignment1.py
+++ Assignment1.py
@@ -1,22 +1,25 @@
 import hashlib
 from datetime import datetime
 
-string = input("Enter a string ")
+input_string = input("Enter a string ")
 
 target = int("0x00000FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF",16)
-main_hexa = hashlib.sha256(str.encode('utf-8')).hexdigest()
+main_hexa = hashlib.sha256(input_string.encode('utf-8')).hexdigest()
 i = 0
-copy = string + str(i)
-start = datetime.now()
+copy_of_input_string = input_string + str(i)
+start_time = datetime.now()
 
-while int(hashlib.sha256(copy.encode('utf-8')).hexdigest(),16)>=target:
+while int(hashlib.sha256(copy_of_input_string.encode('utf-8')).hexdigest(),16)>=target:
     i+=1
-    copy = string + str(i)
+    copy_of_input_string = input_string + str(i)
 
-end = datetime.now()
+end_time = datetime.now()
 
-print(f"Number to be appended to string : {i}")
+print(f"Number to be appended to input string : {i}")
+print(f"Hash of resulting string : {hashlib.sha256(copy_of_input_string.encode('utf-8')).hexdigest()}")
+print(f"Time taken for program to run : {end_time-start_time}")
 
-print(f"Time taken for program to run : {end-start}")
-
-print(f"Hash of resulting string : {hashlib.sha256(copy.encode('utf-8')).hexdigest()}")
+"""After importing the required libraries, we ask for string input from user.
+We then initialise the target variable which is a integer (converted from hexadecimal string to base 10 integer)
+main_hexa variable is the sha256 output of the input string and copy variable is the copy of the input string. 
+We append numbers starting from 0 to the copy string and convert it to hex and check whether it is less than target variable. If yes, that is the number we are looking for."""
